39a6dca1144d5875f277e06ed576aa4be28a7ca9
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2025-05-18 15:25:51 -0700

    Initial Commit

---
+++ PlantCommunityApp/PlantCommunityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantCommunityAppApp.swift
+//  PlantCommunityApp
+//
+//  Created by Jeremy Manlangit on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PlantCommunityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
