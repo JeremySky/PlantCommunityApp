3981d868a28e6dd2caf52eda2d41c55de2309ae3
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2025-05-21 14:14:40 -0700

    Feature: Implement tab navigation with dummy views

--- PlantCommunityApp/HomeView.swift
+++ PlantCommunityApp/HomeView.swift
@@ -1,15 +1,28 @@
 import SwiftUI
 
-struct MainView: View {
+struct HomeView: View {
     var body: some View {
         TabView {
-            MainFeedView()
-            ForYouView()
-            ProfileView()
+            //PostsView
+            Tab("Posts", systemImage: "house.fill") {
+                Text("PostsView")
+            }
+            //BlogsView
+            Tab("Blogs", systemImage: "book.fill") {
+                Text("BlogsView")
+            }
+            //NewContent
+            Tab("New Content", systemImage: "plus") {
+                Text("NewContentVew")
+            }
+            //ProfileView
+            Tab("Profile", systemImage: "person.circle") {
+                Text("ProfileView")
+            }
         }
     }
 }
 
 #Preview {
-    MainView()
+    HomeView()
 }
